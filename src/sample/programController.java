3c37e40f947fc3f0e2edfe7f4b2533a93db8b272
Ozzyz <aasmundbrekke@gmail.com>
2016-03-14 13:38:07 +0100

    Fixed programcontroller

---
+++ src/sample/programController.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Ozzy on 14.03.2016.
+ */
+public class programController extends SceneController implements Initializable{
+    @FXML
+    ListView<String> muscleListView;
+
+    public void populateListView(){
+        ObservableList<String> items = FXCollections.observableArrayList (
+                "Test1", "Test2", "ASD", "123");
+        muscleListView.setItems(items);
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        populateListView();
+    }
+}
