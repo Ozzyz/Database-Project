d0b50290d4ed4325bd29c18af3f25297e7ae1b6c
Ozzyz <aasmundbrekke@gmail.com>
2016-03-14 11:57:58 +0100

    Fixe

--- src/sample/sessionController.java
+++ src/sample/sessionController.java
@@ -6,5 +6,8 @@ import javafx.event.ActionEvent;
  * Created by Ozzy on 14.03.2016.
  */
 public class sessionController extends SceneController{
-
+    @Override
+    public void backButtonClicked(ActionEvent evt){
+        super.backButtonClicked(evt);
+    }
 }
