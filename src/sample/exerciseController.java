266ccac8d2f022625ff7df12580b6adf08323d2d
Johannes Ervum <johannes_ervum@hotmail.com>
2016-03-14 14:21:58 +0100

    Fikset comboboksene i Exercise og Session

--- src/sample/exerciseController.java
+++ src/sample/exerciseController.java
@@ -1,13 +1,35 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-/**
- * Created by Ozzy on 14.03.2016.
- */
-public class exerciseController extends SceneController {
+public class exerciseController extends SceneController implements Initializable {
+
+    @FXML
+    ComboBox<String> gruppeComboBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle res) {
+        populateComboBox();
+    }
+
+    public void populateComboBox(){
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Item1");
+        list.add("Item2");
+        list.add("Item3");
+        ObservableList<String> obList = FXCollections.observableList(list);
+        gruppeComboBox.setItems(obList);
+    }
 
     public void registerExercise(ActionEvent actionEvent) {
     }
--- src/sample/sessionController.java
+++ src/sample/sessionController.java
@@ -1,36 +1,50 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 
 import java.io.IOException;
 import java.lang.reflect.Array;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.ResourceBundle;
 
 /**
  * Created by Ozzy on 14.03.2016.
  */
 
-// TODO: Get a list of all programs, add it to combobox
 
-public class sessionController extends SceneController{
+public class sessionController extends SceneController implements Initializable{
+
     @FXML
     ComboBox<String> programComboBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle res) {
+        populateComboBox();
+    }
+
+// TODO: Get a list of all programs, add it to combobox
+    public void populateComboBox(){
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Item1");
+        list.add("Item2");
+        list.add("Item3");
+        ObservableList<String> obList = FXCollections.observableList(list);
+        programComboBox.setItems(obList);
+    }
+
     @Override
     public void backButtonClicked(ActionEvent evt){
         super.backButtonClicked(evt);
     }
 
     public void addRegisterButtonClicked(ActionEvent actionEvent) {
-    }
-
-    public void populateComboBox(Collection<String> programList){
-        programComboBox.getItems().addAll(programList);
-    }
-
-    public void addCancelButtonClicked(ActionEvent actionEvent) {
-
     }
 }
