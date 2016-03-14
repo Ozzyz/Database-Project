71c8495b35ab26e8cc052977ca1806816bbe203a
Ozzyz <aasmundbrekke@gmail.com>
2016-03-14 11:03:21 +0100

    Small fixes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,28 +1,41 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Controller {
+    @FXML
     public void addSessionButtonClicked(ActionEvent actionEvent) {
-        // Change scene to where we register session
-        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
         try {
-            Parent newscene = FXMLLoader.load(getClass().getResource("session.fxml"));
+            changeScene("session.fxml", actionEvent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    public void addExerciseButtonClicked(ActionEvent actionEvent) {
+        try {
+            changeScene("session.fxml", actionEvent);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void addExerciseButtonClicked(ActionEvent actionEvent) {
-    }
+    public void changeScene(String fxml_resource, ActionEvent evt) throws IOException {
 
-    public void changeScene(){
+        // Change scene to where we register session
+        Stage currentStage = (Stage) ((Node) evt.getSource()).getScene().getWindow();
+
+        Parent newroot = FXMLLoader.load(getClass().getResource("session.fxml"));
+
+        currentStage.setScene(new Scene(newroot, 600,600));
 
     }
 }
