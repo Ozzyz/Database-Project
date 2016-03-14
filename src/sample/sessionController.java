ddbb100811ab41c376cee720355874e5f84f41ee
Johannes Ervum <johannes_ervum@hotmail.com>
2016-03-14 12:06:30 +0100

    test

--- src/sample/sessionController.java
+++ src/sample/sessionController.java
@@ -1,7 +1,29 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
 /**
  * Created by Ozzy on 14.03.2016.
  */
 public class sessionController {
+    @FXML
+    public void addCancelButtonClicked(ActionEvent actionEvent){
+        try {
+            Controller.changeScene("sample.fxml", actionEvent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void addRegisterButtonClicked(ActionEvent actionEvent){
+        try {
+            changeScene("session.fxml", actionEvent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
