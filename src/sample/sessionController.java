7b18d17ae6eaa03a17d53dea3998b2b2712d4da3
Ozzyz <aasmundbrekke@gmail.com>
2016-03-14 12:49:54 +0100

    small foxes

--- src/sample/sessionController.java
+++ src/sample/sessionController.java
@@ -2,14 +2,22 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Created by Ozzy on 14.03.2016.
  */
 
+// TODO: Get a list of all programs, add it to combobox
+
 public class sessionController extends SceneController{
+    @FXML
+    ComboBox<String> programComboBox;
     @Override
     public void backButtonClicked(ActionEvent evt){
         super.backButtonClicked(evt);
@@ -18,6 +26,11 @@ public class sessionController extends SceneController{
     public void addRegisterButtonClicked(ActionEvent actionEvent) {
     }
 
+    public void populateComboBox(Collection<String> programList){
+        programComboBox.getItems().addAll(programList);
+    }
+
     public void addCancelButtonClicked(ActionEvent actionEvent) {
+
     }
 }
