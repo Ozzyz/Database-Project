da68639ee51f6a0d78d371bf66d3addca2d0630d
Ozzyz <aasmundbrekke@gmail.com>
2016-03-14 10:53:34 +0100

    Small stuff

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,28 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Controller {
+    public void addSessionButtonClicked(ActionEvent actionEvent) {
+        // Change scene to where we register session
+        Stage currentStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        try {
+            Parent newscene = FXMLLoader.load(getClass().getResource("session.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addExerciseButtonClicked(ActionEvent actionEvent) {
+    }
+
+    public void changeScene(){
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent mainScene = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Parent addSession = FXMLLoader.load(getClass().getResource("session.fxml"));
+        Parent addSession =
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
