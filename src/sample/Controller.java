5094a188c6199a469b12f7fcb7de4feef95aae2b
Johannes Ervum <johannes_ervum@hotmail.com>
2016-03-15 11:53:17 +0100

    Nye sider og update controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,18 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller extends SceneController{
     @FXML
@@ -36,5 +40,19 @@ public class Controller extends SceneController{
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    public void viewButtonClicked(ActionEvent actionEvent) {
+        try {
+            changeScene("view.fxml", actionEvent);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void populateListView(ArrayList<String> list, ListView<String> listView){
+        ObservableList<String> obList = FXCollections.observableList(list);
+        listView.setItems(obList);
     }
 }
---
+++ src/sample/viewController.java
@@ -0,0 +1,39 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
+
+import java.awt.datatransfer.StringSelection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class viewController extends Controller implements Initializable{
+
+    @FXML
+    ListView<String> sessionListView;
+    @FXML
+    ListView<String> exerciseListView;
+    @FXML
+    TableView<String> infoTableView;
+
+    public void backButtonClickd(ActionEvent actionEvent) {
+        super.backButtonClicked(actionEvent);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //TODO Fetch data from db
+        ArrayList<String> liste = new ArrayList<>();
+        liste.add("Item1");
+        liste.add("Item2");
+        liste.add("Item3");
+        super.populateListView(liste, sessionListView);
+        super.populateListView(liste, exerciseListView);
+    }
+}
