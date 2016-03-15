a63ff633b14d5bd5ce9adb4f5e3fa8752cb33cd4
Ozzyz <aasmundbrekke@gmail.com>
2016-03-15 12:16:38 +0100

    Updated DBHandler and excercise

--- src/db/DBHandler.java
+++ src/db/DBHandler.java
@@ -15,48 +15,68 @@ public class DBHandler {
 
     }
 
-    public static ResultSet getTables(DatabaseMetaData dbmd) throws SQLException{
-        return dbmd.getTables(null, null, null, null);
+    public ResultSet getTables(DatabaseMetaData dbmd){
+        try {
+            return dbmd.getTables(null, null, null, null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
-    public static ResultSet getColums(String tableName, DatabaseMetaData dbmd) throws SQLException{
-        return dbmd.getColumns(null, null, tableName, null);
+
+    public ResultSet getColums(String tableName, DatabaseMetaData dbmd){
+        try {
+            return dbmd.getColumns(null, null, tableName, null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
-    public static void leggTilProgram(Connection conn,String navn) throws SQLException{
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO program(navn) VALUES(?)");
-        ps.setString(1, navn);
-        ps.executeUpdate();
-    }
-    public static void leggTilGruppe(Connection conn,String muskelgruppe, String tilhører) throws SQLException{
-        if((tilhører != null)&& !checkIfInDB(conn,"gruppe","muskelgruppe",tilhører)){
-            PreparedStatement ps = conn.prepareStatement("INSERT INTO gruppe(muskelgruppe,Gruppe_GruppeID) VALUES(?,?)");
-            ps.setString(1, tilhører);
-            ps.setNull(2, java.sql.Types.INTEGER);//ikke ferdig
+    public void leggTilProgram(Connection conn,String navn){
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("INSERT INTO program(navn) VALUES(?)");
+            ps.setString(1, navn);
             ps.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        if(!checkIfInDB(conn,"gruppe","muskelgruppe",muskelgruppe)){
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery("Select GruppeID FROM gruppe WHERE muskelgruppe="+'"'+tilhører+'"');
-            int tilhørergruppe;
-            if(rs.next()){
-                tilhørergruppe = rs.getInt("GruppeID");
-                PreparedStatement ps = conn.prepareStatement("INSERT INTO gruppe(muskelgruppe, Gruppe_GruppeID) VALUES(?,?)");
-                ps.setString(1, muskelgruppe);
-                ps.setInt(2, tilhørergruppe);
+
+    }
+    public void leggTilGruppe(Connection conn,String muskelgruppe, String tilhører){
+        try {
+            if ((tilhører != null) && !checkIfInDB(conn, "gruppe", "muskelgruppe", tilhører)) {
+                PreparedStatement ps = conn.prepareStatement("INSERT INTO gruppe(muskelgruppe,Gruppe_GruppeID) VALUES(?,?)");
+                ps.setString(1, tilhører);
+                ps.setNull(2, java.sql.Types.INTEGER);//ikke ferdig
                 ps.executeUpdate();
             }
-            else{
-                PreparedStatement ps = conn.prepareStatement("INSERT INTO gruppe(muskelgruppe, Gruppe_GruppeID) VALUES(?,?)");
-                ps.setString(1, muskelgruppe);
-                ps.setNull(2, java.sql.Types.INTEGER);
-                ps.executeUpdate();
+            if (!checkIfInDB(conn, "gruppe", "muskelgruppe", muskelgruppe)) {
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery("Select GruppeID FROM gruppe WHERE muskelgruppe=" + '"' + tilhører + '"');
+                int tilhørergruppe;
+                if (rs.next()) {
+                    tilhørergruppe = rs.getInt("GruppeID");
+                    PreparedStatement ps = conn.prepareStatement("INSERT INTO gruppe(muskelgruppe, Gruppe_GruppeID) VALUES(?,?)");
+                    ps.setString(1, muskelgruppe);
+                    ps.setInt(2, tilhørergruppe);
+                    ps.executeUpdate();
+                } else {
+                    PreparedStatement ps = conn.prepareStatement("INSERT INTO gruppe(muskelgruppe, Gruppe_GruppeID) VALUES(?,?)");
+                    ps.setString(1, muskelgruppe);
+                    ps.setNull(2, java.sql.Types.INTEGER);
+                    ps.executeUpdate();
+                }
+            } else {
+                System.out.println("finnes i databasen");
             }
-        }
-        else{
-            System.out.println("finnes i databasen");
+        }catch(Exception e){
+            e.printStackTrace();
         }
     }
 
-    public static ArrayList<String> getProgramNames() throws SQLException {
+    public ArrayList<String> getProgramNames() throws SQLException {
         PreparedStatement ps = conn.prepareStatement("select Navn from Program");
         ArrayList<String> programNames = new ArrayList<>();
         ResultSet res = ps.executeQuery();
@@ -67,7 +87,7 @@ public class DBHandler {
         return programNames;
     }
 
-    public static ArrayList<String> getExerciseNames() throws SQLException {
+    public ArrayList<String> getExerciseNames() throws SQLException {
         PreparedStatement ps = conn.prepareStatement("select Navn from Øvelse");
         ArrayList<String> exerciseNames = new ArrayList<>();
         ResultSet res = ps.executeQuery();
@@ -78,7 +98,7 @@ public class DBHandler {
         return exerciseNames;
     }
 
-    public static ArrayList<String> getMuscleGroupNames() throws SQLException {
+    public ArrayList<String> getMuscleGroupNames() throws SQLException {
         PreparedStatement ps = conn.prepareStatement("select MuskelGruppe from Gruppe");
         ArrayList<String> muscleGroupNames = new ArrayList<>();
         ResultSet res = ps.executeQuery();
@@ -93,27 +113,20 @@ public class DBHandler {
     public static ResultSet getAllRowsFromTable(Connection conn,String table) throws SQLException{
         Statement stmt = conn.createStatement();
         return stmt.executeQuery("SELECT * FROM "+table);
+    }
 
-    }
-    public static boolean checkIfInDB(Connection conn,String table, String column, String tuplevalue) throws SQLException{
-        PreparedStatement ps = conn.prepareStatement("SELECT * FROM "+table+" WHERE "+ column+"=?");
-        ps.setString(1, tuplevalue);
-        ResultSet rs = ps.executeQuery();
-        if (rs.next()){
-            return true;
-        }
-        else return false;
-    }
-    public static void main(String[] args) {
-        DBHandler dbh = new DBHandler();
-        ArrayList<String> list = null;
+    public boolean checkIfInDB(Connection conn,String table, String column, String tuplevalue){
+        PreparedStatement ps = null;
         try {
-            list = getProgramNames();
+            ps = conn.prepareStatement("SELECT * FROM "+table+" WHERE "+ column+"=?");
+            ps.setString(1, tuplevalue);
+            ResultSet rs = ps.executeQuery();
+            // Return if we got results
+            return rs.next();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        for (String s: list) {
-            System.out.println(s);
-        }
+        return false;
     }
+
 }
